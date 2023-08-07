65bfcae1d1e6089e337a5632eb026069149bc2b3
jaekim3220 <jehyunkim9@gmail.com>
2023-08-07 18:09:33 +0900

    모듈 변경에 의한 파일 내용 변경 및 업데이트

--- helper.py
+++ helper.py
@@ -48,6 +48,126 @@ def replaceMissingValue(df, strategy='mean'):
     return re_df
 
 
+#--------------------------------------------------
+# 분산분석(일원분산분석 (One-way ANOVA)) 모듈
+#--------------------------------------------------
+
+# 분산분석(일원분산분석 (One-way ANOVA)) 모듈2 -> 추가내용 : DataFrame 생성용
+def normality_test(*any):
+    """
+    분산분석을 수행하기 위한 정규성을 검정 한다.
+    Parameters
+    -------
+    - any: 필드들
+    Returns
+    -------
+    - df: 검정 결과 데이터 프레임
+    """
+    names = []
+
+    result = {
+        'statistic': [],
+        'p-value': [],
+        'result': []
+    }
+    for i in any:
+        s, p = shapiro(i)
+        result['statistic'].append(s)
+        result['p-value'].append(p)
+        result['result'].append(p > 0.05)
+        names.append(('정규성', 'shapiro', i.name))
+
+    for i in any:
+        s, p = normaltest(i)
+        result['statistic'].append(s)
+        result['p-value'].append(p)
+        result['result'].append(p > 0.05)
+        names.append(('정규성', 'normaltest', i.name))
+
+    n = len(any)
+
+    for i in range(0, n):
+        j = i + 1 if i < n - 1 else 0
+
+        s, p = ks_2samp(any[i], any[j])
+        result['statistic'].append(s)
+        result['p-value'].append(p)
+        result['result'].append(p > 0.05)
+        names.append(('정규성', 'ks_2samp', f'{any[i].name} vs {any[j].name}'))
+
+    return DataFrame(result, index=MultiIndex.from_tuples(names, names=['condition', 'test', 'field']))
+
+# 분산분석(일원분산분석 (One-way ANOVA)) 모듈1
+def equal_variance_test(*any):
+    """
+    분산분석을 수행하기 위한 등분산성을 검정 한다.
+    Parameters
+    -------
+    - any: 필드들
+    Returns
+    -------
+    - df: 검정 결과 데이터 프레임
+    """
+    # statistic=1.333315753388535, pvalue=0.2633161881599037
+    s1, p1 = bartlett(*any)
+    s2, p2 = fligner(*any)
+    s3, p3 = levene(*any)
+    names = []
+    for i in any:
+        names.append(i.name)
+
+    fix = " vs "
+    name = fix.join(names)
+    index = [['등분산성', 'Bartlett', name], ['등분산성', 'Fligner', name], ['등분산성', 'Levene', name]]
+
+    df = DataFrame({
+        'statistic': [s1, s2, s3],
+        'p-value': [p1, p2, p3],
+        'result': [p1 > 0.05, p2 > 0.05, p3 > 0.05]
+    }, index=MultiIndex.from_tuples(index, names=['condition', 'test', 'field']))
+
+    return df
+
+def independence_test(*any):
+    """
+    분산분석을 수행하기 위한 독립성을 검정한다.
+    Parameters
+    -------
+    - any: 필드들
+    Returns
+    -------
+    - df: 검정 결과 데이터 프레임
+    """
+    df = DataFrame(any).T
+    result = chi2_contingency(df)
+    names = []
+    for i in any:
+        names.append(i.name)
+    fix = " vs "
+    name = fix.join(names)
+
+    index = [['독립성', 'Chi2', name]]
+
+    df = DataFrame({
+        'statistic': [result.statistic],
+        'p-value': [result.pvalue],
+        'result': [result.pvalue > 0.05]
+    }, index=MultiIndex.from_tuples(index, names=['condition', 'test', 'field']))
+
+    return df
+
+def all_test(*any):
+    """
+    정규성, 등분산성, 독립성을 모두 검정한다.
+    Parameters
+    -------
+    - any: 필드들
+    Returns
+    -------
+    - df: 검정 결과 데이터 프레임
+    """
+    return concat([normality_test(*any), equal_variance_test(*any), independence_test(*any)])
+
 
 def getIq(field, isPrint=True):
     """
@@ -912,7 +1032,7 @@ class LogitResult:
     def odds_rate_df(self, value):
         self._odds_rate_df = value
 
-def myOls(data, y, x, subset=None):
+def myLogit(data, y, x, subset=None):
     """
     로지스틱 회귀분석을 수행한다.
 
